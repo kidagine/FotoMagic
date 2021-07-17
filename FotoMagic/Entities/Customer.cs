f67b03e440e584c535bcd7e7ab844aede53165bd
David Kalatzis <davidkalatzis1999@hotmail.com>
2021-07-17 11:02:33 +0200

    Initial commit for new version.

---
+++ FotoMagic/Entities/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fotomagic.Entities
+{
+	class Customer
+	{
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OwedMoney { get; set; }
+
+        public Customer(int id, string firstName, string lastName, string owedMoney)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            OwedMoney = owedMoney;
+        }
+    }
+}
---
+++ FotoMagic/SplashScreen.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fotomagic
+{
+	/// <summary>
+	/// Interaction logic for SplashScreen.xaml
+	/// </summary>
+	public partial class SplashScreen : Window
+	{
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+
+        public SplashScreen()
+        {
+            InitializeComponent();
+            StartAnimation();
+
+            dispatcherTimer.Tick += new EventHandler(DispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 3);
+            dispatcherTimer.Start();
+        }
+        public void StartAnimation()
+        {
+            imgSplashScreen.Opacity = 0;
+            DoubleAnimation animation = new DoubleAnimation(1, TimeSpan.FromSeconds(1f));
+            imgSplashScreen.BeginAnimation(ImageBrush.OpacityProperty, animation);
+        }
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            ProductsScreen productsScreen = new ProductsScreen();
+            this.Hide();
+            productsScreen.Show();
+
+            dispatcherTimer.Stop();
+		}
+    }
+}
